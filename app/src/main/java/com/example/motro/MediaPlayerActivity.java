6a33e6d4b224868c2db1e261706ab322022b6f3e
jiajing.lv <lvi_166@163.com>
2026-08-19 15:02:06 +0800

    1.delete useless dependencies

--- app/src/main/java/com/example/motro/MediaPlayerActivity.java
+++ app/src/main/java/com/example/motro/MediaPlayerActivity.java
@@ -149,7 +149,7 @@ public class MediaPlayerActivity extends AppCompatActivity {
 
     private void initDefaults() {
         String initialUrl = getIntent().getStringExtra(EXTRA_URL);
-        controlsBinding.urlEditText.setText(TextUtils.isEmpty(initialUrl) ? "rtsp://192.168.1.101:554/main.mov" : initialUrl);
+        controlsBinding.urlEditText.setText(TextUtils.isEmpty(initialUrl) ? "rtsp://192.168.1.101:556/main.mov" : initialUrl);
         controlsBinding.timeoutEditText.setText(String.valueOf(DEFAULT_TIMEOUT_MS));
         audioSwitch.setChecked(false);
         reconnectSwitch.setChecked(true);
